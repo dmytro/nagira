c08535bf96a82265cd776ca510de9d8f802e7a51
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-06-03 23:46:57 +0900

    page load specs

--- spec/01_nagira_response_spec.rb
+++ spec/01_nagira_response_spec.rb
@@ -11,12 +11,36 @@ describe Nagira do
     @app ||= Nagira
   end
 
-  %w{ config objects status }.each do |page|
-    it "should load /#{page} page" do 
-      get "/#{page}"
-      last_response.should be_ok
-    end
-    
-  end
 
+  FORMATS = %w{ xml yaml json}
+  TYPES   = %w{ state list}
+
+  context "simple page load" do
+    %w{ config objects status }.each do |page|
+      it "should load /#{page} page" do 
+        get "/#{page}"
+        pending "No output for config page yet" if page == 'config'
+        last_response.should be_ok
+      end
+      
+
+        # Check 3 different formats
+        FORMATS.each do |format|
+          
+          it "should load '#{page}.#{format}' page" do
+            get "/#{page}.#{format}"
+            pending "No output for config page yet" if page == 'config'
+            last_response.should be_ok
+          end
+
+        #         TYPES.each do |type|
+        #           it "should load '#{page}/#{type}.#{format}' page" do
+        #             get "/#{page}/#{type}.#{format}"
+        #             pending "No output for config page yet" if page == 'config'
+        #             last_response.should be_ok
+        #           end
+        #         end
+      end
+    end
+  end
 end

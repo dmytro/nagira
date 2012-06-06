cd76f6b5036951b849ac1173f74ccad7270b82ff
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-06-06 10:12:56 +0900

    yard documentation

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@
 $: << File.dirname(__FILE__)
 require 'lib/nagira'
 
+##
+#
+# Note:: Mostly methods with comments Sinatra DSL :before method are not actually
+#        method definitions. They are Sinatra DSL methods
+#        calls. Looking for better means to document DSL methods with
+#        YARD.
+#
 class Nagira < Sinatra::Base
 
   set :app_file, __FILE__
@@ -23,7 +30,11 @@ class Nagira < Sinatra::Base
   #
   # See also comments in config/default.rb file regarding nagios_cfg,
   # status_cfg, objects_cfg.
-
+  #
+  # @method parse_nagios_files  
+  #
+  # Sinatra DSL :before method
+  #
 
   before do 
 
@@ -45,21 +56,63 @@ class Nagira < Sinatra::Base
     @objects   = $nagios[:objects].objects
   end
 
+  ##
+  # @method BEFORE_strip_extensions 
+  #
+  # Sinatra DSL :before method
+  #
   # Strip extension (@format) from HTTP route and set it as instance
-  # variable @format
+  # variable @format. Valid formats are .xml, .json, .yaml. If format
+  # is not specified, it is set to default format
+  # (Nagira.settings.format).
+  #
+  # \@format can be assigned one of the symbols: :xml, :json, :yaml.
+  #
+  # = Examples
+  # 
+  #     GET /objects             # => default format
+  #     GET /objects.json        # => :json
+  #     GET /status/list.yaml    # default format
+  # 
   before do 
     request.path_info.sub!(/#{settings.format_extensions}/, '')
     @format = ($1 || settings.format).to_sym
     content_type "application/#{@format.to_s}"
   end
 
-  # Find output type (@output) if provided: :list, :state or :full
+  ##
+  # @method BEFORE_strip_output_type 
+  #
+  # Sinatra DSL :before method
+  #
+  # Detect and strip output type (@output) from HTTP route if it is
+  # provided. Full list of output tuypes is :list, :state or
+  # :full. 
+  #
+  # Output type in HTTP route can be 'list' or 'state'. If no output
+  # type specfied it is set to :full.
+  #
+  # = Examples
+  #
+  #     GET /objects/list     # => :list
+  #     GET /status/state     # => :state
+  #     GET /status/:hostname # => :full
+  #
+  # @see API
   before do
     request.path_info.sub!(/\/(list|state)$/, '')
     @output = ($1 || :full).to_sym
   end
 
-  # Return 404 if data array is empty.
+  ##
+  # @method AFTER_object_not_found
+  # Sinatra :after DSL method
+  #
+  # If result-set of object/status search is empty return HTTP
+  # 404. This can happen when you are requesting status for not
+  # existing host and/or service.
+  #
+  #
   after do
     if response.body.empty?
       halt [404, {
@@ -70,9 +123,27 @@ class Nagira < Sinatra::Base
     end
   end
 
-  # Routes for configured objects in the system
+  ##
+  # @method GET_config
+  # 
+  # Nagira API
+  # TODO: Not implemented
+  #
+  # Retrieves Nagios configuration hash.
+  # @see API
+  get "/config" do 
 
-  # Full list
+  end
+
+
+  ##
+  # @method GET_objects
+  #
+  # Get full list of Nagios objects. Returns hash containin all
+  # configured objects in Nagios: hosts, hostgroups, services,
+  # contacts. etc.
+  #
+  # Nagira API
   get "/objects" do
     
     body (@output == :list ? 

51cd99b75e8b755097465424cea65709b64fe1a3
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-10-03 10:37:52 +0900

    rename Nagios::ExternalCommands.send to .write

--- app/routes/put.rb
+++ app/routes/put.rb
@@ -1,17 +1,21 @@
 class Nagira < Sinatra::Base
 
-  # Helper to send PUT update to Nagios::ExternalCommands
-  #
-  # @param [Hash] params 
-  # @param [Array] required List of required paramaters for this action
-  # @param [Symbol] action Nagios external command name
-  def put_update action, params
-    
-    if $nagios[:commands].send(params.merge({ :action => action }))
-      { :status => 'OK', :object => params}
-    else
-      { :status => "FAILED", :object => params }
+  # Define helpers for put methods
+  helpers do 
+    # Helper to send PUT update to Nagios::ExternalCommands
+    #
+    # @param [Hash] params 
+    # @param [Array] required List of required paramaters for this action
+    # @param [Symbol] action Nagios external command name
+    def put_update action, params
+      
+      if $nagios[:commands].write(params.merge({ :action => action }))
+        { :status => 'OK', :object => params}
+      else
+        { :status => "FAILED", :object => params }
+      end
     end
   end
+
   
 end

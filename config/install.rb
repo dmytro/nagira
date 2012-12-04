1cb4dc3bed7bb2b3b724bbb69c6eade4c49f2bdc
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-12-04 16:35:28 +0900

    fix Nagira INSTALL

--- config/install.rb
+++ config/install.rb
@@ -1,10 +1,12 @@
 #
 # Installation options for nagira API. All variables are accessible under constall INSTALL
 #
-Nagira::INSTALL = { 
-  :run_as => "nagios",
-  :use_rvm => true,
-  :rvm => '1.9.3',
-  :root => File.dirname( File.dirname(__FILE__)),
-  :log => "var/log/nagira.log"
-}
+class Nagira
+  INSTALL = { 
+    :run_as => "nagios",
+    :use_rvm => true,
+    :rvm => '1.9.3',
+    :root => File.dirname( File.dirname(__FILE__)),
+    :log => "var/log/nagira.log"
+  }
+end

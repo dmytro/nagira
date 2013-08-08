fdd252412420abddb9fbef06a0473295c4133f98
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-08-08 15:34:10 +0900

    Add service|host comments to implemented routes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,8 @@ require 'rack/test'
 IMPLEMENTED = {
   top:            %w{ _api _status _objects _config _runtime},
   output:          %w{ _list _state _full }, # Type of requests as in varaible @output
-  hosts:          %w{ tv archive },
-#  status:         %w{ _services _list _state _full _servicecomments _hostcomments _hosts }
-  status:         %w{ _services _list _state _full }
+  hosts:          %w{ archive tv },
+  status:         %w{ _services _servicecomments _hostcomments }
 }
 
 RANDOM = %w{ _bla _foo bla foo ahdhjdjda }

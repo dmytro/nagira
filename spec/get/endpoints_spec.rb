45f29badaa01e86f4c01645095d1a4174f96ae98
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-08-23 20:33:47 +0900

    Tests for custom hostame regex - WIP, pending.

--- spec/get/endpoints_spec.rb
+++ spec/get/endpoints_spec.rb
@@ -7,20 +7,30 @@ require 'spec_helper'
 shared_examples_for :fail_on_random_url do |base|  
   RANDOM.each do |url|
     ep = "#{base}/#{url}"
-    it "fails on #{ep} string" do
+    it "fails on '#{ep}' string" do
       get ep
       last_response.status.should be 404
     end
   end
 end
 
-shared_examples_for :respond_to_valid_url do |base, urls|
-  urls.each do |url|
-    ep = "#{base}/#{url}"
-    it "responds to #{ep}" do
-      get ep
-      last_response.should be_ok
+shared_examples_for :respond_to_valid_url do |base, urls, custom_regex|
+
+  case urls
+    when Array
+    urls.each do |url|
+      ep = "#{base}/#{url}"
+      it "responds to #{ep}" do
+        get ep
+        last_response.should be_ok
+      end
     end
+    when nil
+      ep = "#{base}"
+      it "responds to #{ep}" do
+        get ep
+        last_response.should be_ok
+      end
   end
 end
 
@@ -41,7 +51,9 @@ describe Nagira do
   
   context "API endpoints" do
     
-    host = IMPLEMENTED[:hosts].first 
+    host   = IMPLEMENTED[:hosts].first 
+    spaces = "host%20with%20space"
+
 
     context :top do 
       it_should_behave_like :respond_to_valid_url,  '', IMPLEMENTED[:top]
@@ -59,14 +71,33 @@ describe Nagira do
       it_should_behave_like :respond_to_valid_url,  "/_status/#{host}", IMPLEMENTED[:status]
       it_should_behave_like :fail_on_random_url,    "/_status/#{host}"
 
+      context "hostname with space" do
+        it_should_behave_like :fail_on_bad_url,  "/_status/#{spaces}"
+      end
+
       context :unknown_host do
         it_should_behave_like :fail_on_bad_url,       '/_status/unknown_host'
         it_should_behave_like :fail_on_random_url,    "/_status/unknown_host"
       end
+    end                         # /_status/:host
 
+    context "/_status/:host/_services" do 
+      it_should_behave_like :respond_to_valid_url,  "/_status/#{host}/_services"
+      it_should_behave_like :respond_to_valid_url,  "/_status/#{host}/_services", ["SSH", "PING"]
+
+      context "hostname with space" do
+        it_should_behave_like :fail_on_bad_url,  "/_status/#{spaces}/_services"
+      end
     end
 
-  end                           # API endpoints
 
+    context "custom hostname regex - host with spaces" do
+        
+      it { pending "Need to figure out how to change hostname regex on the fly" }
+        #it_should_behave_like :respond_to_valid_url,  "/_status/#{spaces}", nil, '\w[(%20)\w\-\.]+'
+        #it_should_behave_like :respond_to_valid_url,  "/_status/#{spaces}/_services"
+    end                         # custom hostname regex
+    
+  end                           # API endpoints
     
 end

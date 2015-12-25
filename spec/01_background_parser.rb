f3be9c26c55346a0b896c74bf9d2cf83ec51d59a
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2015-12-25 10:24:26 +0900

    Fix BG parser spec

--- spec/01_background_parser.rb
+++ spec/01_background_parser.rb
@@ -4,23 +4,23 @@ require 'spec_helper'
 describe "BackgroundParser" do
 
   before {
-    Nagios::BackgroundParser.ttl = 1
-    Nagios::BackgroundParser.start = true
+    Nagira::BackgroundParser.ttl = 1
+    Nagira::BackgroundParser.start = true
   }
 
   it "is configured" do
-    expect(Nagios::BackgroundParser).to be_configured
+    expect(Nagira::BackgroundParser).to be_configured
   end
 
   context "after start" do
 
     before {
-      Nagios::BackgroundParser.run
+      Nagira::BackgroundParser.run
       sleep 0.1
     }
 
     it "is running" do
-      expect(Nagios::BackgroundParser).to be_alive
+      expect(Nagira::BackgroundParser).to be_alive
     end
 
   end
